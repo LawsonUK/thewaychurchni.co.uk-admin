2dde08e5baa5afd7b54c6779940ec1445ee61b73
LawsonUK <chrislawsonuk@hotmail.com>
2020-08-19 20:15:44 +0100

    Adding rest of content types

---
+++ api/blog-post/models/blog-post.js
@@ -0,0 +1,23 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
+ * to customize this model
+ */
+
+const slugify = require("slugify");
+
+module.exports = {
+  beforeSave: async (model) => {
+    if (model.title) {
+      model.slug = slugify(model.title);
+    }
+  },
+  beforeUpdate: async (model) => {
+    if (model.getUpdate() && model.getUpdate().title) {
+      model.update({
+        slug: slugify(model.getUpdate().title),
+      });
+    }
+  },
+};
